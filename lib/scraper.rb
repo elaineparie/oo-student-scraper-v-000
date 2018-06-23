6d77309a269d2f61cd240de2cb605143ffe06a81
Elaine Anderson <eparie@bu.edu>
2018-06-23 17:12:09 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,8 @@ class Scraper
   def self.scrape_index_page(index_url)
 
       doc = Nokogiri::HTML(open("fixtures/student-site/index.html"))
+
+      binding.pry
       #  student = Student.new
 
         #doc.css(".student-card").first
